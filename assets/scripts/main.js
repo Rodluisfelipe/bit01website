b4559084ddb1899dfdba3ab8e6ad3b2def1b1104
FelipeSoftlu <luisroqu@unisabana.edu.co>
2023-05-29 17:40:48 -0500

    changes

---
+++ assets/scripts/main.js
@@ -0,0 +1,8 @@
+
+let formulario=document.getElementById('contact-form');
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault(); // Evita que el formulario se envíe de forma predeterminada
+    
+   formulario.addEventListener('submit', function(event) {
+    window.location.href = "https://rodluisfelipe.github.io/bit01website/sitio_en_contruccion.html";})
+  });
